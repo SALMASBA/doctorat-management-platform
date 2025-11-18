ae42b5603b8124a24bff33d74e019de818b87bc6
SALMASBA <salmasbaa550@gmail.com>
2025-11-18 13:38:01 +0100

    Premier commit

---
+++ Doctorant-Service/src/test/java/com/example/Doctorant_Service/DoctorantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Doctorant_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Thesis-Service/src/test/java/com/example/Thesis_Service/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Thesis_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
